b83577672208754a4cc1d380cd37b84ecbf8a535
Steven R. Baker <steven@stevenrbaker.com>
2022-05-12 21:12:43 +0200

    Initial Commit

---
+++ foobar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  foobar
+//
+//  Created by Steven R. Baker on 2022-05-12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
